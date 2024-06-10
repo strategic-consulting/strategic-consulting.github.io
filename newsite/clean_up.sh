7a886d0ee1b4ca22a968ead9b9656e5581019f49
Catherine Brys <catherine@strategic-consulting.scot>
2024-06-10 19:45:26 +0100

    new site

---
+++ newsite/clean_up.sh
@@ -0,0 +1,7 @@
+  rm republish upload generate_sitemap.pl republish_no_git add_new_p*
+  rm -Rf indexasatAug23/index.html 
+  rm publish*
+  rm -Rf lawfirms/ facilitation/ thirdsector/ 
+  rm -Rf businesses/ coaching/ articles/ drafts/
+  rm -Rf draft/
+  rm stage set_mod_date.pl 
