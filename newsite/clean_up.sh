a12d68a65d5c234ea4ef351337cfeba7f7ebf21e
Catherine Brys <catherine@strategic-consulting.scot>
2024-07-16 13:42:45 +0100

    pre-release 3

--- newsite/clean_up.sh
+++ newsite/clean_up.sh
@@ -1,14 +1,11 @@
 rm republish upload generate_sitemap.pl republish_no_git add_new_p*
-rm -Rf indexasat*/index.html 
+rm -Rf indexasatAug23/index.html 
 rm publish*
-rm -Rf thirdsector/ 
-rm -Rf businesses/  drafts/
+rm -Rf lawfirms/ thirdsector/ 
+rm -Rf businesses/ coaching/ articles/ drafts/
 rm -Rf draft/
 rm stage set_mod_date.pl 
 rm -Rf indexasatAug23/
 rm -Rf */indexold
 rm -Rf */*/indexold
 rm -Rf */indeold
-rm -Rf articles/old
-rm -Rf articles/test
-rm -Rf cases/case*/indexold/*
